def2aac734dc5787f8ee17bd5ce0a92574db142c
Matthew Hart <matthew.hart@palmettohelath.org>
2017-07-14 16:18:06 -0400

    more work

--- src/app/Services/wwdc.service.ts
+++ src/app/Services/wwdc.service.ts
@@ -4,13 +4,13 @@ import {mod} from "../Model/mod";
 @Injectable()
 export class WWDCService {
   mods: mod[] = [
-    new mod("Mod One",),
-    new mod,
-    new mod,
-    new mod,
-    new mod,
-    new mod,
-    new mod,
-    new mod
+    new mod("Mod One"),
+    new mod("Mod Two"),
+    new mod("Mod Three"),
+    new mod("Mod Four"),
+    new mod("Mod Five"),
+    new mod("Mod Six"),
+    new mod("Mod Seven"),
+    new mod("Mod Eight")
   ];
 }
